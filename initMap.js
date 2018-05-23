7debae4be1f196caa644db1b2f63176eb90159ba
davidionita <31045833+davidionita@users.noreply.github.com>
2018-05-23 10:26:40 -0400

    Rollback

--- initMap.js
+++ initMap.js
@@ -7,7 +7,6 @@ let markers = [];
 let before = {};
 let first = true;
 
-
 function initMap() {
     // Create a map object and specify the DOM element for display.
     let centerPos = {lat: 40.900464, lng: -74.0333};
@@ -29,9 +28,6 @@ function bindInfoWindow(marker, map, infowindow, html) {
 }
 
 function setMarkers() {
-
-
-
     let nIcon = {
         url: 'images/yellow_school_bus.png',
         scaledSize: new google.maps.Size(40, 40)
@@ -44,15 +40,15 @@ function setMarkers() {
         url: 'images/red_school_bus.png',
         scaledSize: new google.maps.Size(40, 40)
     };
-    for (let i = 0; i < jsTowns.length; i++) {
-        if (jsTowns[i].location === "") continue;
+    for (let i = 0; i < newThing.length; i++) {
+        if (newThing[i].location === "") continue;
 
         let contentString = '<div id="content">'+
             '<div id="siteNotice">'+
             '</div>'+
-            '<h4 id="fourthHeading" class="fourthHeading">' + jsTowns[i].name + '</h4>'+
+            '<h4 id="fourthHeading" class="fourthHeading">' + newThing[i].name + '</h4>'+
             '<div id="bodyContent">'+
-            '<p>' + jsTowns[i].location + '</p>'+
+            '<p>' + newThing[i].location + '</p>'+
             '</div>'+
             '</div>';
 
@@ -62,53 +58,35 @@ function setMarkers() {
 
         let iconChoice = {};
 
-        if (searchResult.length < 47 && searchResult.some(e => e.name === jsTowns[i].name)) {
+        if (searchResult.length < 47 && searchResult.some(e => e.name === newThing[i].name)) {
             iconChoice = sIcon;
         } else {
             iconChoice = nIcon
         }
 
         let marker = new google.maps.Marker({
-            position: posCoordDict[jsTowns[i].location],
+            position: posCoordDict[newThing[i].location],
             map: map,
-            title: jsTowns[i].name,
+            title: newThing[i].name,
             icon: iconChoice
         });
 
         bindInfoWindow(marker, map, infoWindow);
 
-        if (before[jsTowns[i].name] !== undefined){
+        if (before[newThing[i].name] !== undefined){
             marker.setMap(null);
         } else {
-            before[jsTowns[i].name] = marker;
+            before[newThing[i].name] = marker;
         }
 
-        console.log(jsFavorite);
-
-
-        if (marker.getPosition().lat() !== before[jsTowns[i].name].getPosition().lat() ||
-            marker.getPosition().lng() !== before[jsTowns[i].name].getPosition().lng() ||
-            marker.getIcon().url !== before[jsTowns[i].name].getIcon().url) {
-            before[jsTowns[i].name].setMap(null);
-            before[jsTowns[i].name] = marker;
-            before[jsTowns[i].name].setMap(map);
+        if (marker.getPosition().lat() !== before[newThing[i].name].getPosition().lat() ||
+            marker.getPosition().lng() !== before[newThing[i].name].getPosition().lng() ||
+            marker.getIcon().url !== before[newThing[i].name].getIcon().url) {
+            before[newThing[i].name].setMap(null);
+            before[newThing[i].name] = marker;
+            before[newThing[i].name].setMap(map);
         }
 
     }
 }
 
-function geolocation () {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            let pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-
-            let marker = new google.maps.Marker({
-                position: pos,
-                map: map,
-            });
-        })
-    }
-}
